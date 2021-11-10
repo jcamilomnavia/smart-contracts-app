970900da9aeab7f982af6c58fa56cc232613e271
Juan Camilo Marin Navia <jcamilomnavia@hotmail.com>
2021-11-10 07:11:11 -0500

    Created truffle sdk

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,61 +2,25 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
-import SdkLoader from 'views/components/sdk-loader';
-import getWeb3 from './getWeb3';
-import p2Contract from './contracts/P2.json';
-
 import Routes from 'routes';
 import routesConfig from 'routes/config';
 import { store, persistor } from 'store';
 import history from 'store/history';
+import TruffleSDK from 'utils/truffle-sdk';
+import SdkLoader from 'views/components/sdk-loader';
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
   componentDidMount = async () => {
-    try {
-      const web3 = await getWeb3();
-      const accounts = await web3.eth.getAccounts();
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = p2Contract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        p2Contract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-      this.setState({ web3, accounts, contract: instance });
-    } catch (error) {
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
-  };
-
-  createTask = async (energy, price) => {
-    const { accounts, contract } = this.state;
-    await contract.methods.createTask(energy, price).send({ from: accounts[0] });
-  };
-
-  findBestBuy = async (id) => {
-    const { accounts, contract } = this.state;
-    await contract.methods.findBestBuy(id).send({ from: accounts[0] });
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-    await contract.methods.set(5).send({ from: accounts[0] });
-    const response = await contract.methods.get().call();
-    this.setState({ storageValue: response });
+    await TruffleSDK.init();
   };
 
   render() {
-    if (!this.state.web3) {
+    if (!TruffleSDK.getWeb3()) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <Provider store={store}>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {TruffleSDK.getStorage()}</div>
 
         <PersistGate loading={null} persistor={persistor}>
           <SdkLoader>
---
+++ client/src/utils/truffle-sdk.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-underscore-dangle */
+import getWeb3 from './getWeb3';
+import p2Contract from '../contracts/P2.json';
+
+const TruffleSDK = {
+  _storageValue: 0,
+  _web3: null,
+  _accounts: null,
+  _contract: null,
+  _error: null,
+
+  init: async () => {
+    try {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = p2Contract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        p2Contract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+      TruffleSDK._accounts = accounts;
+      TruffleSDK._web3 = web3;
+      TruffleSDK._contract = instance;
+    } catch (error) {
+      TruffleSDK._error = error;
+      console.error(error);
+    }
+  },
+
+  setStorage: (value) => {
+    TruffleSDK._storageValue = value;
+  },
+  getStorage: () => {
+    return TruffleSDK._storageValue;
+  },
+
+  getError: () => {
+    return TruffleSDK._error;
+  },
+
+  runExample: async () => {
+    const { accounts, contract } = TruffleSDK;
+    await contract.methods.set(5).send({ from: accounts[0] });
+    const response = await contract.methods.get().call();
+    this.setState({ storageValue: response });
+  },
+
+  createTask: async (energy, price) => {
+    const { accounts, contract } = TruffleSDK;
+    await contract.methods
+      .createTask(energy, price)
+      .send({ from: accounts[0] });
+  },
+
+  findBestBuy: async (id) => {
+    const { accounts, contract } = TruffleSDK;
+    await contract.methods.findBestBuy(id).send({ from: accounts[0] });
+  },
+};
+
+export default TruffleSDK;
